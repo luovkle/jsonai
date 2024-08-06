cea3ddad8c2dc4e290e95fc3caad1d31e62d4293
luovkle <roberto.silva.dev@protonmail.com>
2024-08-06 13:59:45 -0600

    🥅 Add custom error pages

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -1,10 +1,18 @@
 import sentry_sdk
-from flask import Flask
+from flask import Flask, render_template
 
 from app.cache import init_cache_db
 from app.config import settings
 from app.db import init_db
 from app.views import bp as views_bp
+
+
+def page_not_found(_):
+    return render_template("404.html"), 404
+
+
+def internal_server_error(_):
+    return render_template("500.html"), 500
 
 
 def create_app():
@@ -18,7 +26,9 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY=settings.APP_SECRET_KEY,
     )
-    app.register_blueprint(views_bp)
     init_db(app)
     init_cache_db(app)
+    app.register_blueprint(views_bp)
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
     return app
