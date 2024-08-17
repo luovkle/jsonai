3267f8b3d16b977423202c99df5d2b153da625b0
luovkle <roberto.silva.dev@protonmail.com>
2024-08-16 20:49:45 -0600

    🔨 Add djlint to scripts

--- web_app/scripts/format.sh
+++ web_app/scripts/format.sh
@@ -7,3 +7,5 @@ autoflake --in-place .
 black .
 isort .
 docformatter --in-place .
+djlint --profile=jinja --reformat --quiet --indent 2 --preserve-blank-lines \
+  --close-void-tags app/templates/
--- web_app/scripts/lint.sh
+++ web_app/scripts/lint.sh
@@ -7,3 +7,6 @@ black --check .
 isort --check-only .
 flake8 .
 docformatter --check .
+djlint --profile=jinja --check --indent 2 --preserve-blank-lines \
+  --close-void-tags app/templates/
+djlint --lint app/templates/
