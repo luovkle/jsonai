504f8599004c789840c09e7c8a79612d050f7137
luovkle <roberto.silva.dev@protonmail.com>
2024-08-19 18:37:22 -0600

    ✨ Add API errors

--- web_app/app/views/api.py
+++ web_app/app/views/api.py
@@ -1,9 +1,35 @@
 from flask import Blueprint, jsonify
+from werkzeug.exceptions import (
+    BadRequest,
+    InternalServerError,
+    NotFound,
+    UnprocessableEntity,
+)
 
 from app.db import db_delete, db_read, db_show
 from app.exceptions import DocumentNotFoundError
 
 bp = Blueprint("api", __name__, url_prefix="/api")
+
+
+@bp.errorhandler(400)
+def bad_request(e: BadRequest):
+    return jsonify(detail=e.description), 400
+
+
+@bp.errorhandler(404)
+def not_found(e: NotFound):
+    return jsonify(detail=e.description), 404
+
+
+@bp.errorhandler(422)
+def unprocessable_entity(e: UnprocessableEntity):
+    return jsonify(detail=e.description), 422
+
+
+@bp.errorhandler(500)
+def internal_server_error(_: InternalServerError):
+    return jsonify(detail="Internal Server Error"), 500
 
 
 @bp.get("/<topic_id>")
